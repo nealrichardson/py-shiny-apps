17fd801a07ce25dea9e66152d2b8d3a9f6f32049
Neal Richardson <neal.p.richardson@gmail.com>
2023-05-26 08:23:49 -0400

    Refactor to simplify wtf UI

--- wtf/app.py
+++ wtf/app.py
@@ -1,49 +1,30 @@
+from pathlib import Path
 import random
+
 
 from shiny import App, render, ui, reactive
 
-my_css = """
-html {
-  height: 100%;
-}
 
-body {
-    height: 100%;
-    width: 100%;
-    text-align: center;
-    background-color: yellow;
-    font-size: 24px;
-    font-family: 'Jost', sans-serif;
-}
+def local_file(path):
+    return Path(__file__).parent / path
 
-h2 {
-    font-size: 48px;
-    font-family: 'Lobster', sans-serif;
-}
 
-.container {
-    height: 100%;
-    width: 100%;
-    display: table;
-    table-layout: fixed;
-}
+def click_button_on_load(id):
+    "Use jQuery to fire event `id` on shiny:connected"
+    
+    script = f"""
+        $(document).on('shiny:connected', function(event) {{
+        Shiny.setInputValue(
+            '{id}', 
+            1
+        );
+        }});
+    """
+    return ui.tags.script(ui.HTML(script))
 
-.inside {
-    vertical-align: middle;
-    display: table-cell;
-}
-"""
 
 app_ui = ui.page_fixed(
-    ui.tags.link(
-        rel="stylesheet",
-        href="https://fonts.googleapis.com/css?family=Jost"
-    ),
-    ui.tags.link(
-        rel="stylesheet",
-        href="https://fonts.googleapis.com/css?family=Lobster"
-    ),
-    ui.tags.style(my_css),
+    ui.include_css(local_file("style.css")),
     ui.div(
         {"class": "inside"},
         ui.h2("WTF is my open-source strategy?"),
@@ -57,24 +38,13 @@ app_ui = ui.page_fixed(
             ui.output_text("maximize", inline=True),
         ),
         ui.br(),
-        ui.input_action_button(
-            "redo",
-            ui.output_text("button_msg")
-        ),
+        ui.input_action_button("redo", ui.output_text("button_msg")),
     ),
-    ui.tags.script(ui.HTML("""
-$(document).on('shiny:connected', function(event) {
-  Shiny.setInputValue(
-    'redo', 
-    1
-  );
-});
-    """))
+    click_button_on_load("redo"),
 )
 
 
 def server(input, output, session):
-    
     verbs = [
         "Drive",
         "Disrupt",
@@ -114,13 +84,7 @@ def server(input, output, session):
         "ABI stability",
         "lock-in",
     ]
-    buttons = [
-        "Ship it!",
-        "YOLO!",
-        "Rebase!",
-        "Pull!",
-        "Fork!"
-    ]
+    buttons = ["Ship it!", "YOLO!", "Rebase!", "Pull!", "Fork!"]
 
     def sample_one(wordlist):
         return wordlist[random.randint(0, len(wordlist) - 1)]
@@ -136,13 +100,13 @@ def server(input, output, session):
     @reactive.event(input.redo)
     def noun():
         return sample_one(nouns)
-       
+
     @output
     @render.text
     @reactive.event(input.redo)
     def action():
         return sample_one(by_doing)
-    
+
     @output
     @render.text
     @reactive.event(input.redo)
@@ -154,5 +118,6 @@ def server(input, output, session):
     @reactive.event(input.redo)
     def button_msg():
         return sample_one(buttons)
+
 
 app = App(app_ui, server)
